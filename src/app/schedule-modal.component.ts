891d85330e32541aaba03427e6087fcfd3ec349b
Tamas <tvirag@gmail.com>
2017-09-26 21:25:15 -0600

    Work done on Schedule

--- src/app/schedule-modal.component.ts
+++ src/app/schedule-modal.component.ts
@@ -15,6 +15,7 @@ import { Observable } from "rxjs/Observable";
 
 export class ScheduleModalComponent {
 
+    nextid: number;
     todos: Todo[];
     assignees: Observable<string[]>;
     tasks: Observable<string[]>;
@@ -23,8 +24,59 @@ export class ScheduleModalComponent {
     assigneeName: string;
     taskName: string;
 
+    constructor() { 
+        this.todos = [];
 
+   
+          this.todos.push({
+              id: 1,
+              name: "Take out garbage",
+              dateCreated: new Date('2017-05-01'),
+              dateComplete: null,
+              daysToComplete: null,
+              assignee: "Me",
+              done: false
+          });
+          this.todos.push({
+              id: 2,
+              name: "Do dishes",
+              dateCreated: new Date('2017-07-11'),
+              dateComplete: null,
+              daysToComplete: null,
+              assignee: "Me",
+              done: false
+          });
+          this.todos.push({
+              id: 3,
+              name: "Make bed",
+              dateCreated: new Date('2017-08-20'),
+              dateComplete: null,
+              daysToComplete: null,
+              assignee: "Me",
+              done: false
+          });
+  
     }
 
+    complete (todo: Todo) {
+        todo.dateComplete = new Date();
+        todo.done = true;
+        todo.daysToComplete = Math.floor(((todo.dateComplete.getTime() - todo.dateCreated.getTime()) / (1000 * 60 * 60 * 24) ));
+    }
+
+    remove(todo: Todo) {
+        let index = this.todos.findIndex(t=>t.id === todo.id);
+        if (index >= 0) {
+            this.todos.splice(index, 1);
+        }
+    }
+
+    all(): Todo[] {
+        return this.todos;
+    }
+
+    find(id:number): Todo {
+        return this.todos.find(t=>t.id == id);
+    }
 
 }
